d102a65a3ecab52fb7628471b0c2cca665399660
Alex Grand <namelost@protonmail.com>
2020-06-03 18:08:21 +0300

    ex11-15

---
+++ ex12/ex12.py
@@ -0,0 +1,6 @@
+name = input("What's your name? ")
+age = input(f"How old are you {name}? ")
+height = input("How tall are you? ")
+weight = input("How much do you weigh? ")
+
+print(f"So {name}, you're {age} old, {height} tall and {weight} heavy.")
